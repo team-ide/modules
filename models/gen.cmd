84faf049dff4149c804bdf36d240e2d88478d387
朱亮 <teamide@163.com>
2026-04-09 13:59:01 +0800

    提交 模块模型

---
+++ models/gen.cmd
@@ -0,0 +1,6 @@
+@echo on
+
+cd %~dp0
+
+coos gen -m ./ -o ../ -r ../repository
+
---
+++ models/gen.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+# 获取脚本所在的目录
+SCRIPT_DIR="$(dirname "$0")"
+
+# 切换到脚本所在的目录
+cd "$SCRIPT_DIR"
+
+coos gen -m ./ -o ../ -r ../repository
